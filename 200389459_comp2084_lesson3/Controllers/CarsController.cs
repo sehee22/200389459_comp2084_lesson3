50fc9310d2d811a3666fb6d4838467c1a62d9e53
Sehee Park <200389459@student.georgianc.on.ca>
2019-05-28 17:32:13 -0400

    first commit

---
+++ 200389459_comp2084_lesson3/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _200389459_comp2084_lesson3.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
